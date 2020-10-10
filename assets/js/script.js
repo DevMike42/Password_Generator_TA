9a4f8f22ac37e0f136950340476bed9d0d8c74d0
Mike Rivera <michaelstephenrivera@gmail.com>
2020-10-09 18:00:02 -0700

    Created event listener for generateBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+// Target HTML
+// ====================================================
+const generateBtn = document.querySelector('#generate');
+
+
+// Global Variables
+// ====================================================
+
+
+
+
+// Functions
+// ====================================================
+
+// Generate password
+const generatePassword = function () {
+  console.log('Working!');
+
+};
+
+
+
+
+// Event Listeners
+// ====================================================
+generateBtn.addEventListener('click', generatePassword);
